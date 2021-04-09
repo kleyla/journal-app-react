79b9f7d705dab859f03e8ca6c6087552e6302756
kleyla <kleyla01@live.com>
2021-04-09 17:48:00 -0400

    logout, orivate and public routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
-  Route,
   BrowserRouter as Router,
   Switch,
   Redirect,
 } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { firebase } from "./../firebase/firebase-config";
 import { JournalScreen } from "../components/journal/JournalScreen";
 import { AuthRouter } from "./AuthRouter";
+import { login } from "../actions/auth";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
+  const dispatch = useDispatch();
+  const [checking, setChecking] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // la sgte linea crea un obserbable que se puede disparar mas de una vez
+    firebase.auth().onAuthStateChanged((user) => {
+      // console.log(user);
+      if (user?.uid) {
+        dispatch(login(user.uid, user.displayName));
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setChecking(false);
+    });
+  }, [dispatch, setChecking, setIsLoggedIn]);
+
+  if (checking) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/auth" component={AuthRouter} />
-          <Route exact path="/" component={JournalScreen} />
+          <PublicRoute
+            path="/auth"
+            component={AuthRouter}
+            isAuthenticated={isLoggedIn}
+          />
+          <PrivateRoute
+            exact
+            isAuthenticated={isLoggedIn}
+            path="/"
+            component={JournalScreen}
+          />
 
           <Redirect to="/auth/login" />
         </Switch>
