11f27f4a1b9069161eb0b60870c424d84e5c7448
kleyla <kleyla01@live.com>
2021-04-12 18:31:41 -0400

    agregando variables de entorno desarrollo y produccion

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -2,6 +2,8 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
+console.log(process.env);
+
 const firebaseConfig = {
   apiKey: "AIzaSyA8t_8wTm64T8UAc_dJjbT_YAVlzVgvyUI",
   authDomain: "journal-app-react-redux.firebaseapp.com",
@@ -10,6 +12,13 @@ const firebaseConfig = {
   messagingSenderId: "1025993761594",
   appId: "1:1025993761594:web:84b1a35b1618c8438aa603",
 };
+if (process.env.NODE_ENV === "test") {
+  // TESTING
+  // firebase.initializeApp(firebaseConfigTesting);
+} else {
+  // PRODUCTION
+  // firebase.initializeApp(firebaseConfig);
+}
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
