a196256679e1067fa25604d128e2828507a872c1
kleyla <kleyla01@live.com>
2021-04-12 23:08:00 -0400

    components tested

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -14,7 +14,7 @@ export const JournalEntry = ({ id, date, title, body, url }) => {
 
   return (
     <div
-      className="journal_entry pointer animate__animated animate__fadeIn animate__faster"
+      className="journal__entry pointer animate__animated animate__fadeIn animate__faster"
       onClick={handleEntryClick}
     >
       {url && (
